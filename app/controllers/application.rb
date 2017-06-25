582458c297f34399e81b47a6994274cb39b274c9
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-06-25 15:34:16 -0700

    Implement outline of controller actions and index, show actions (without views)

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,3 +1,46 @@
 get '/' do
   "Hello world!"
 end
+
+# index
+get '/contacts' do
+  @contacts = Contact.all
+  @contacts.map { |contact| contact.email }.to_s
+end
+
+# new
+get '/contacts/new' do
+  "This is the contacts new action"
+end
+
+# create
+post '/contacts' do
+  "This is the contacts create action"
+end
+
+# show
+get '/contacts/:id' do
+  puts params
+  @contact = Contact.find(params[:id])
+  @contact.email
+end
+
+### All of the following actions are referred to as member actions, since they act on a single member
+# edit
+get '/contacts/:id/edit' do
+  "This is the contacts edit action"
+end
+
+# update
+patch '/contacts/:id' do
+  "This is the contacts update action"
+end
+
+put '/contacts/:id' do
+  "This is the contacts update action"
+end
+
+# delete
+delete '/contacts/:id'do
+  "This is the contacts delete action"
+end
